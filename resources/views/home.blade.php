1dbef2d5b4de137236acb66d0d19ebff22894a94
Cosmin <cosminzorr@gmail.com>
2018-04-20 23:42:58 +0300

    Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,47 @@
 
 @section('content')
 <div class="container">
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="btn btn-danger">New</div>
+                    <div class="form-group">
+                        <input type="text" name="join-room-name" placeholder="Room name" class="form-control">
+                        <div class="btn btn-success">Join</div>
+                    </div>
 
-                    You are logged in!
+                    <h4 class="card-title">Rooms</h4>
+                    <table data-toggle="table" data-height="280" data-mobile-responsive="true" data-sort-order="desc" class="table">
+                        <thead>
+                        <tr>
+                            <th data-field="name" data-sortable="true"> Name </th>
+                            <th data-field="stargazers_count" data-sortable="true" data-width="100"> Stars </th>
+                            <th data-field="forks_count" data-sortable="true" data-width="100"> Forks </th>
+                            <th data-field="description" data-sortable="true"> Description </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                            </tr>
+                            <tr>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                                <td>asdasd</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
