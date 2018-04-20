45766e2085ae258f396566c01030674ff5aaabd9
Cosmin <cosminzorr@gmail.com>
2018-04-20 23:26:27 +0300

    Migrations for rooms and languages + seed

---
+++ database/migrations/2018_04_20_200732_create_rooms.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRooms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('known_language')->unsigned()->nullable();
+            $table->integer('foreign_language')->unsigned()->nullable();
+            $table->integer('max_players')->default(2);
+            $table->integer('created_by')->unsigned()->nullable();
+            
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
