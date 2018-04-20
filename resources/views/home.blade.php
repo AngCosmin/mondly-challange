9d2acd299596e6a1ae02b1eec3fcc0fa6a4a455c
Cosmin <cosminzorr@gmail.com>
2018-04-20 23:52:21 +0300

    New room modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,13 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <div class="btn btn-danger">New</div>
                     <div class="form-group">
                         <input type="text" name="join-room-name" placeholder="Room name" class="form-control">
                         <div class="btn btn-success">Join</div>
                     </div>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Create
+                    </button>
 
                     <h4 class="card-title">Rooms</h4>
                     <table data-toggle="table" data-height="280" data-mobile-responsive="true" data-sort-order="desc" class="table">
@@ -44,6 +46,29 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Create room modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create room</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
